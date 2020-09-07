e3bf637fc55419ec40209b05cc2b512bd4e94cf3
unknown <rodrigomarcolino@gmail.com>
2020-09-07 14:23:34 +1200

    Firs Commit

---
+++ Computer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExample
+{
+    abstract class Computer
+    {
+
+        protected string _name = "Unknow";
+        public bool isOn { get; private set;  }
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+
+        public Computer(String name)
+        {
+            this.name = name;
+        }
+
+        protected void TurOn()
+        {
+            isOn = true;
+        }
+
+        protected void TurOff()
+        {
+            isOn = false;
+        }
+
+        public void TogglePower()
+        {
+            if (isOn)
+            {
+                TurOff();
+            } else
+            {
+                TurOn();
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using OOPExample;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var comp = new Computer("Generic Computer");
+
+        Console.WriteLine($"Computer Type - {comp.GetType()}");
+        
+        //comp.name = "Generic computer";
+
+        var isComputer = comp is Computer;
+        Console.WriteLine($"Is Comuter - {isComputer}");
+        Console.WriteLine($"Computer Name - {comp.name}");
+
+        comp.TogglePower();
+        Console.WriteLine($"Computer is ON - {comp.isOn}");
+
+        comp.TogglePower();
+        Console.WriteLine($"Computer is ON - {comp.isOn}");
+
+        Console.Read();
+    }
+}
